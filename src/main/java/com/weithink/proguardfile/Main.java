ab87214a53a0bfb93fd67fd8f0182c27944ffbb0
weithink <760540105@qq.com>
2020-05-12 14:41:30 +0800

    111

---
+++ src/main/java/com/weithink/proguardfile/Main.java
@@ -0,0 +1,12 @@
+package com.weithink.proguardfile;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        ChineseProguard.start();
+        new SpecificCharacterProguard("proguard-o0O.txt", '0', 'o', 'O', '8', '〇').start();
+        new SpecificCharacterProguard("proguard-1il.txt", '1', 'i', 'l', 'L', 'I', '丨').start();
+        new SpecificCharacterProguard("proguard-socialism.txt", "abcdefghijklmnopqrstuvwxyz".toCharArray()).start();
+    }
+}
